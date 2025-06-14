f3283034038128c1d21a4407c499f7d410a80787
dmanslick <dvvshah@gmail.com>
2025-06-13 21:17:37 -0400

    Added more controls, moon, and saturn's rings

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import * as THREE from 'three'
-import { createEarth, createJupiter, createMars, createMercury, createNeptune, createSaturn, createSun, createUranus, createVenus } from './objects'
+import { createEarth, createJupiter, createMars, createMercury, createMoon, createNeptune, createSaturn, createSun, createUranus, createVenus } from './objects'
 import './style.css'
 import { TrackballControls } from 'three/examples/jsm/Addons.js'
 
 const scene = new THREE.Scene()
+
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1e8)
 camera.position.z = 1000
 
@@ -23,14 +24,13 @@ const controls = new TrackballControls(camera, renderer.domElement)
 // controls.staticMoving = true
 // controls.dynamicDampingFactor = 0.3
 
-let renderLoopCallbacks: (() => void)[] = []
-export const addRenderLoopCallback = (callback: (() => void)) => {
+let renderLoopCallbacks: ((timeStep: number) => void)[] = []
+export const addRenderLoopCallback = (callback: ((timeStep: number) => void)) => {
     renderLoopCallbacks.push(callback)
 }
 
 let timeStep = 0
 let timeStepIncrement = 0.0001
-export const getTimeStep = () => timeStep
 
 const Sun = createSun(scene)
 const Mercury = createMercury(scene)
@@ -41,8 +41,9 @@ const Jupiter = createJupiter(scene)
 const Saturn = createSaturn(scene)
 const Uranus = createUranus(scene)
 const Neptune = createNeptune(scene)
+const Moon = createMoon(scene, Earth)
 
-const objects = [Sun, Mercury, Venus, Earth, Mars, Jupiter, Saturn, Uranus, Neptune]
+const objects = [Sun, Mercury, Venus, Earth, Moon, Mars, Jupiter, Saturn, Uranus, Neptune]
 
 let focusedObjectIndex = 0
 let relativeCameraOffset = new THREE.Vector3(0, 0, 100)
@@ -84,7 +85,7 @@ const renderLoop = () => {
     controls.update()
     relativeCameraOffset = camera.position.clone().sub(controls.target)
 
-    renderLoopCallbacks.forEach(cb => cb())
+    renderLoopCallbacks.forEach(cb => cb(timeStep))
     renderer.render(scene, camera)
 }
 
@@ -98,4 +99,24 @@ speedSlider?.addEventListener("change", (e: Event) => {
     const percent = Number(input.value)
     timeStepIncrement = 0.0001 * percent
     speedDisplay.innerText = percent.toString()
+})
+
+let stoppedTimeIncrementStep = 0
+
+const stopButton = document.getElementById('stop-button')
+const resumeButton = document.getElementById('resume-button')
+
+stopButton?.addEventListener('click', () => {
+    stoppedTimeIncrementStep = timeStepIncrement
+    timeStepIncrement = 0
+    speedSlider.disabled = true
+})
+
+resumeButton?.addEventListener('click', () => {
+    timeStepIncrement = stoppedTimeIncrementStep
+    speedSlider.disabled = false
+})
+
+window.addEventListener('resize', () => {
+    renderer.setSize(window.innerWidth, window.innerHeight)
 })
--- src/objects.ts
+++ src/objects.ts
@@ -1,39 +1,51 @@
 import * as THREE from 'three'
-import { EarthConfig, JupiterConfig, MarsConfig, MercuryConfig, NeptuneConfig, SaturnConfig, SunConfig, UranusConfig, VenusConfig } from './configs'
-import { addRenderLoopCallback, getTimeStep } from './main'
+import { EarthConfig, JupiterConfig, MarsConfig, MercuryConfig, MoonConfig, NeptuneConfig, SaturnConfig, SunConfig, UranusConfig, VenusConfig } from './configs'
+import { addRenderLoopCallback } from './main'
 
 function createBasicPlanet(scene: THREE.Scene, radius: number, distance: number, color: THREE.ColorRepresentation) {
     const planetGeometry = new THREE.SphereGeometry(radius)
-    const planetMaterial = new THREE.MeshStandardMaterial({
-        color, emissive: color, emissiveIntensity: 0.5, roughness: 0.4,
-        metalness: 0.1
-    })
+    const planetMaterial = new THREE.MeshStandardMaterial({ color, emissive: color, emissiveIntensity: 0.5, roughness: 0.4, metalness: 0.1 })
     const planet = new THREE.Mesh(planetGeometry, planetMaterial)
     planet.position.set(0, distance, 0)
     scene.add(planet)
 
-    const orbitCurve = new THREE.EllipseCurve(
-        0, 0,                  // center
-        distance, distance,    // xRadius, yRadius
-        0, 2 * Math.PI,        // startAngle, endAngle
-        false,                 // clockwise
-        0                      // rotation
-    )
+    createOrbitOutline(scene, 0, 0, distance, distance, 'red')
 
-    const orbitPoints = orbitCurve.getPoints(100)
-    const orbit3DPoints = orbitPoints.map(p => new THREE.Vector3(p.x, p.y, 0))
-    const orbitGeometry = new THREE.BufferGeometry().setFromPoints(orbit3DPoints)
-    const orbitMaterial = new THREE.LineBasicMaterial({ color: 'red' })
-    const orbitEllipse = new THREE.LineLoop(orbitGeometry, orbitMaterial)
-    scene.add(orbitEllipse)
-
-
-    addRenderLoopCallback(() => {
-        const timeStep = getTimeStep()
+    addRenderLoopCallback((timeStep) => {
         planet.position.set(distance * Math.cos(timeStep * (1000 / distance)), distance * Math.sin(timeStep * (1000 / distance)), 0)
     })
 
     return planet
+}
+
+function createOrbitOutline(scene: THREE.Scene, centerX: number, centerY: number, radiusX: number, radiusY: number, color: THREE.ColorRepresentation) {
+    const orbitCurve = new THREE.EllipseCurve(centerX, centerY, radiusX, radiusY, 0, 2 * Math.PI,)
+    const orbitPoints = orbitCurve.getPoints(250)
+    const orbit3DPoints = orbitPoints.map(p => new THREE.Vector3(p.x, p.y, 0))
+    const orbitGeometry = new THREE.BufferGeometry().setFromPoints(orbit3DPoints)
+    const orbitMaterial = new THREE.LineBasicMaterial({ color })
+    const orbitEllipse = new THREE.LineLoop(orbitGeometry, orbitMaterial)
+    scene.add(orbitEllipse)
+    return () => scene.remove(orbitEllipse)
+}
+
+function createBasicSatellite(scene: THREE.Scene, parent: THREE.Mesh<THREE.SphereGeometry, THREE.MeshStandardMaterial, THREE.Object3DEventMap>, radius: number, distance: number, color: THREE.ColorRepresentation) {
+    const satelliteGeometry = new THREE.SphereGeometry(radius)
+    const satelliteMaterial = new THREE.MeshBasicMaterial({ color })
+    const satellite = new THREE.Mesh(satelliteGeometry, satelliteMaterial)
+    scene.add(satellite)
+
+    let removeLastOrbitOutline = createOrbitOutline(scene, parent.position.x, parent.position.y, distance, distance, 'magenta')
+
+    addRenderLoopCallback((timeStep) => {
+        removeLastOrbitOutline()
+        const parentX = parent.position.x
+        const parentY = parent.position.y
+        removeLastOrbitOutline = createOrbitOutline(scene, parentX, parentY, distance, distance, 'magenta')
+        satellite.position.set(parentX - (distance * Math.cos(timeStep * (1000 / distance))), parentY - (distance * Math.sin(timeStep * (1000 / distance))), 0) // https://www.desmos.com/calculator/imgfwntxx4
+    })
+
+    return satellite
 }
 
 export function createSun(scene: THREE.Scene) {
@@ -60,15 +72,27 @@ export function createEarth(scene: THREE.Scene) {
 }
 
 export function createMars(scene: THREE.Scene) {
-    return createBasicPlanet(scene, MarsConfig.radius, MarsConfig.distance, 'orange')
+    return createBasicPlanet(scene, MarsConfig.radius, MarsConfig.distance, '#b37255')
 }
 
 export function createJupiter(scene: THREE.Scene) {
-    return createBasicPlanet(scene, JupiterConfig.radius, JupiterConfig.distance, 'tan')
+    return createBasicPlanet(scene, JupiterConfig.radius, JupiterConfig.distance, '#e6aa5c')
 }
 
 export function createSaturn(scene: THREE.Scene) {
-    return createBasicPlanet(scene, SaturnConfig.radius, SaturnConfig.distance, 'tan')
+    const saturn = createBasicPlanet(scene, SaturnConfig.radius, SaturnConfig.distance, 'tan')
+    const saturnRingGeometry = new THREE.RingGeometry(SaturnConfig.radius + 60, SaturnConfig.radius + 110)
+    const saturnRingMaterial = new THREE.MeshBasicMaterial({ color: 'gray', side: 2, transparent: true, opacity: 0.75 })
+    const saturnRing = new THREE.Mesh(saturnRingGeometry, saturnRingMaterial)
+    scene.add(saturnRing)
+
+    saturnRing.rotateX(Math.PI * 0.05)
+
+    addRenderLoopCallback(() => {
+        saturnRing.position.set(saturn.position.x, saturn.position.y, saturn.position.z)
+    })
+
+    return saturn
 }
 
 export function createUranus(scene: THREE.Scene) {
@@ -77,4 +101,8 @@ export function createUranus(scene: THREE.Scene) {
 
 export function createNeptune(scene: THREE.Scene) {
     return createBasicPlanet(scene, NeptuneConfig.radius, NeptuneConfig.distance, 'blue')
+}
+
+export function createMoon(scene: THREE.Scene, earth: THREE.Mesh<THREE.SphereGeometry, THREE.MeshStandardMaterial, THREE.Object3DEventMap>) {
+    return createBasicSatellite(scene, earth, MoonConfig.radius, 75, 'gray')
 }
